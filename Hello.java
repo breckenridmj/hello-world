32bd84aa5d4d57ac2f2cbbe0c094b91d172c1508
breckenridmj <59925642+breckenridmj@users.noreply.github.com>
2020-01-15 10:23:43 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+   /*********************************************************************
+   * Hello.java
+   * Malcolm Breckenridge
+   * 1/15/2020
+   * CMSC 255 Section 003
+   **********************************************************************/
+
+public class Hello {
+	public static void main(String[]args) {
+		System.out.println("<Malcolm Breckenridge>");
+		System.out.println("<Hello.java>");
+		System.out.println("<java>");
+		System.out.println("1/15/2020");
+		System.out.println("CMSC 255 Section 003");
+		System.out.println();
+	
+	}
+}
