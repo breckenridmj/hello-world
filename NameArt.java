17f576a07d843e099f17c7d6801d9bb7577fecaf
breckenridmj <59925642+breckenridmj@users.noreply.github.com>
2020-01-17 13:32:31 -0500

    Add files via upload
    Name Art

---
+++ NameArt.java
@@ -0,0 +1,40 @@
+/*************************************************************************
+NameArt.java
+*************************************************************************
+* Programming Project - Name in Code
+* This project outputs my name in a unique way on the display screen
+* Malcolm Breckenridge
+* 1/16/2020
+* CMSC 255 Section 003
+************************************************************************/
+
+
+public class NameArt 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Malcolm Breckenridge");
+		System.out.println("Name Art");
+		System.out.println("Displays My Name In Code");
+		System.out.println("January 16, 2020");
+		System.out.println("CMSC 255 Section 003");
+ 		
+		System.out.println("                                                                                               ");
+ 		System.out.println("                                                                                               ");
+		System.out.println("                                                                                               "); 
+		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+ 		System.out.println("^^ _________________________________________________________________________________________ ^^");
+		System.out.println("^^|                                                                                         |^^");
+		System.out.println("^^|       M     M           A      L         C C C     O O O    L             M     M       |^^");
+		System.out.println("^^|      M M   M M         A A     L       C         O       O  L            M M   M M      |^^");
+		System.out.println("^^|     M   M M   M       A   A    L       C         O       O  L           M   M M   M     |^^");
+		System.out.println("^^|    M     M     M     A A A A   L       C         O       0  L          M     M     M    |^^");
+		System.out.println("^^|   M             M   A       A  L L L L   C C C     O O O    L L L L   M             M   |^^");
+		System.out.println("^^|                                                                                         |^^");
+		System.out.println("^^|_________________________________________________________________________________________|^^");
+		System.out.println("^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+	}
+}
+
+
+
